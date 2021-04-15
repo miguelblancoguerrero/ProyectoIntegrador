fbda42dfcb904e1cda12e6ab97ebcd3d6563cdfb
miguelblancoguerrero <mabg0992@gmail.com>
2021-04-14 20:11:09 -0500

    Ajustes primario administrativa personas

--- ProyectoIntegrador/Migrations/Configuration.cs
+++ ProyectoIntegrador/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace ProyectoIntegrador.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             ContextKey = "ProyectoIntegrador.Models.ApplicationDbContext";
         }
 
