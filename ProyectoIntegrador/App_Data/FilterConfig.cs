cc9c10f540fb0b23fa20a17514368ebe683837cf
miguelblancoguerrero <mabg0992@gmail.com>
2021-04-19 08:36:53 -0500

    Ajustes Por Mal marge

---
+++ ProyectoIntegrador/App_Data/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using ProyectoIntegrador.App_Start.Filters;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoIntegrador
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new SessionFilter());
+        }
+    }
+}
---
+++ ProyectoIntegrador/App_Data/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ProyectoIntegrador
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                name: "Administrativo",
+                url: "administrativos/{controller}/{action}/{id}",
+                defaults: new { controller = "Personas", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "ProyectoIntegrador.Controllers.Administrativos" }
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
