4abb98338ce46d0cd443a2a4a06d024560a68b3a
camiloquintana <82551432+camiloquintana@users.noreply.github.com>
2021-04-23 08:28:10 -0500

    Arreglo migracion
    Se añadio la migración.

--- ProyectoIntegrador/Migrations/202104231325568_ajuste-dep-mun.cs
+++ ProyectoIntegrador/Migrations/202104231325568_ajuste-dep-mun.cs
@@ -3,16 +3,18 @@ namespace ProyectoIntegrador.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class ajustedepartamento : DbMigration
+    public partial class ajustedepmun : DbMigration
     {
         public override void Up()
         {
+            AlterColumn("dbo.municipios", "nombre", c => c.String(nullable: false, maxLength: 30));
             AlterColumn("dbo.departamentos", "nombre", c => c.String(nullable: false, maxLength: 225));
         }
         
         public override void Down()
         {
             AlterColumn("dbo.departamentos", "nombre", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.municipios", "nombre", c => c.String(nullable: false, maxLength: 20));
         }
     }
 }
